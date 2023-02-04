0d19b684368c3dc3785b9933ad069a67fed363e3
aun mirza <aunabbasmirza@gmail.com>
2023-02-04 01:25:34 -0500

    ChartJS in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import LineChart from './components/LineChart';
 
 function App() {
+
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+
+  const userData = {
+    labels: data.map(row => row.year),
+    datasets: [
+      {
+        label: 'Users Gained',
+        data: data.map(row => row.count),
+        backgroundColor : ["#000000"],
+        borderColor : ["#f3ba2f"]
+      }
+    ]
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="line-chart">
+        <LineChart chartData={userData} />
+      </div>
     </div>
   );
 }
