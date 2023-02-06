079f17c3be84d587669ec43aa5dc36a96d142b7b
aun mirza <aunabbasmirza@gmail.com>
2023-02-05 23:35:43 -0500

    Added variable values

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import LineChart from './components/LineChart';
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
+  let data = [
+    { year: 2010, count: 12 },
+    { year: 2011, count: 42 },
+    { year: 2012, count: 30 },
+    { year: 2013, count: 72 },
+    { year: 2014, count: 55},
+    { year: 2015, count: 83 },
+    { year: 2016, count: 74 },
   ];
 
-  const userData = {
+  const [userData, setUserData] = useState(
+  {
     labels: data.map(row => row.year),
     datasets: [
       {
@@ -23,7 +25,29 @@ function App() {
         borderColor : ["#f3ba2f"]
       }
     ]
-  }
+  })
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      data = data.map(row => {
+        row.count = Math.floor(Math.random() * 101);
+        return row;
+      });
+      console.log(data);
+      setUserData(userData => ({
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Users Gained',
+            data: data.map(row => row.count),
+            backgroundColor : ["#000000"],
+            borderColor : ["#f3ba2f"]
+          }
+        ]
+    }));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="App">
